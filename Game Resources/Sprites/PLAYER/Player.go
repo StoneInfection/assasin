8b76abb662b64ba8fb86cd725aa2ce279a4b8378
Omelchenko Michael <socanime@gmail.com>
2023-03-27 14:01:57 +0300

    Fix Coll Mob

--- Game Resources/Sprites/PLAYER/Player.go
+++ Game Resources/Sprites/PLAYER/Player.go
@@ -3,7 +3,7 @@ components {
   component: "/Game Resources/Sprites/PLAYER/Player.sprite"
   position {
     x: 0.0
-    y: 32.0
+    y: 31.0
     z: 0.0
   }
   rotation {
@@ -38,12 +38,11 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"ground\"\n"
-  "mask: \"mob\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
+  "      x: 1.0\n"
   "      y: 31.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -56,8 +55,56 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 12.0725\n"
+  "  data: 11.7935\n"
   "  data: 30.7185\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "CollisionAttack"
+  type: "collisionobject"
+  data: "collision_shape: \"/Game Resources/Environment/Level1.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"PlayerAttack\"\n"
+  "mask: \"Mob\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 23.0\n"
+  "      y: 31.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 10.0\n"
+  "  data: 30.6715\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"

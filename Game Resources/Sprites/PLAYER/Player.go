d20ba09dbcd81c7acc55404c1590997b02dc9a0d
StoneInfection <jokjoli777@gmail.com>
2023-04-19 02:14:16 +0300

    lifebar

--- Game Resources/Sprites/PLAYER/Player.go
+++ Game Resources/Sprites/PLAYER/Player.go
@@ -38,6 +38,7 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"ground\"\n"
+  "mask: \"ship\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
